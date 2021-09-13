f858e09fd646ea3333ce7e5cbc002837cf46a53a
haruki1008 <takaharuki1008@gmail.com>
2021-09-13 17:33:38 +0000

    change chats_controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,75 +1,54 @@
 class ChatsController < ApplicationController
   before_action :set_room
-  before_action :set_chat, only: %i[ show edit update destroy ]
-
-  # GET /chats or /chats.json
-  def index
-    @chats = @room.chats.all
-  end
-
-  # GET /chats/1 or /chats/1.json
-  def show
-  end
+  before_action :set_chat, only: [:show, :edit, :update, :destroy]
+  
 
   # GET /chats/new
   def new
-    @chat = @room.chats.new
+   @chat = @room.chats.new
   end
 
   # GET /chats/1/edit
   def edit
   end
-
-  # POST /chats or /chats.json
+ 
+  # POST /chats
   def create
-    @chat = @room.chats.new(chat_params)
+   @chat = @room.chats.new(chat_params)
 
-    respond_to do |format|
-      if @chat.save
-        format.html { redirect_to room_chats_path, notice: "Chat was successfully created." }
-        format.json { render :show, status: :created, location: @chat }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @chat.errors, status: :unprocessable_entity }
-      end
+    if @chat.save
+     @status = true
+    else
+     @status = false
     end
   end
 
-  # PATCH/PUT /chats/1 or /chats/1.json
+  # PATCH/PUT /chats/1
   def update
-    respond_to do |format|
-      if @chat.update(chat_params)
-        format.html { redirect_to @chat, notice: "Chat was successfully updated." }
-        format.json { render :show, status: :ok, location: @chat }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @chat.errors, status: :unprocessable_entity }
-      end
+    if @chat.update(chat_params)
+      @status = true
+    else
+       @status = false
     end
   end
 
-  # DELETE /chats/1 or /chats/1.json
+  # DELETE /chats/1
   def destroy
     @chat.destroy
-    respond_to do |format|
-      format.html { redirect_to chats_url, notice: "Chat was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_room
-      @room = Room.find_by(id: params[:room_id])
-      
+   @room = Room.find_by(id: params[:room_id])
+   redirect_to(rooms_url, alert: "ERROR!!") if @room.blank?
     end
     
     def set_chat
-      @chat = @room.Chat.find_by(params[:id])
+      @chat = @room.Chat.find_by( params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def chat_params
-      params.require(:chat).permit(:name, :content, :image)
+    params.require(:chat).permit(:name, :content, :image)
     end
+  
 end
