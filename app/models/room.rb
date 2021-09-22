df96079487d9dbb7376c83baec06cf0b8a1fac73
EC2 Default User <ec2-user@ip-172-31-40-27.ap-northeast-1.compute.internal>
2021-09-22 17:04:25 +0000

    tag途中

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
     has_many :chats
     validates :title, presence: true
+    
+    has_many :tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_111050) do
+ActiveRecord::Schema.define(version: 2021_09_22_163715) do
 
   create_table "chats", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 2021_08_19_111050) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
