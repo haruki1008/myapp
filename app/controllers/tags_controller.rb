3b131b83e733b6f9d9239d91468f0e2511c31705
EC2 Default User <ec2-user@ip-172-31-40-27.ap-northeast-1.compute.internal>
2021-09-22 18:34:24 +0000

    tag途中

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,5 @@
 class TagsController < ApplicationController
-  before_action :set_tag, only: %i[ show edit update destroy ]
+  before_action :set_tag, only: [:show, :edit, :update, :destroy]
 
   # GET /tags or /tags.json
   def index
@@ -23,43 +23,34 @@ class TagsController < ApplicationController
   def create
     @tag = Tag.new(tag_params)
 
-    respond_to do |format|
+    
       if @tag.save
-        format.html { redirect_to @tag, notice: "Tag was successfully created." }
-        format.json { render :show, status: :created, location: @tag }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
+      @status = true
+    else
+      @status = false
     end
   end
+  
 
   # PATCH/PUT /tags/1 or /tags/1.json
   def update
-    respond_to do |format|
-      if @tag.update(tag_params)
-        format.html { redirect_to @tag, notice: "Tag was successfully updated." }
-        format.json { render :show, status: :ok, location: @tag }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
+    if @tag.update(tag_params)
+     @status = true
+    else
+     @status = false
     end
   end
 
   # DELETE /tags/1 or /tags/1.json
   def destroy
     @tag.destroy
-    respond_to do |format|
-      format.html { redirect_to tags_url, notice: "Tag was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_tag
-      @tag = Tag.find(params[:id])
+      @tag = @room.tags.find_by(id: params[:id])
     end
 
     # Only allow a list of trusted parameters through.
