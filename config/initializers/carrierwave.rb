b444a8ddf010a1c5d53c5400ac2dfedd9e21213a
EC2 Default User <ec2-user@ip-172-31-40-27.ap-northeast-1.compute.internal>
2021-09-28 19:12:16 +0000

    s3登録途中

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,17 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.storage :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_directory  = 's3bucket1008'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['Access_key_id'],
+    aws_secret_access_key: ENV['Secret_access_key'],
+    region: 'ap-northeast-1',
+    path_style: true
+  }
+
+end
