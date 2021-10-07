e95883482c7ccdce9731bca88dd142ed9756e355
EC2 Default User <ec2-user@ip-172-31-40-27.ap-northeast-1.compute.internal>
2021-10-07 09:59:30 +0000

    s3登録途中

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,14 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
 CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = 's3bucket1008'
+  config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['Access_key_id'],
-    aws_secret_access_key: ENV['Secret_access_key'],
-    region: 'ap-northeast-1',
-    path_style: true
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV['Access_key_id'],  # required
+    aws_secret_access_key: ENV['Secret_access_key'],     # required
+    region:                'ap-northeast-1',                   # optional, defaults to 'us-east-1' オハイオ＝us-east-2
+    #host:                  's3.example.com',             # optional, defaults to nil
+    #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-
+  config.fog_directory  = 'myappbucket1008'                # required
+  config.fog_public     = false                                                 # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
